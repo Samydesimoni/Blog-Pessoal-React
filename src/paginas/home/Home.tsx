b9e82a4f15783ab114ee5baa7b33b9068771a1a5
Samy de Simoni <sssouza.samy@gmail.com>
2022-08-25 12:37:13 -0300

    Criação dos componentes Home e estilização  utilizando CSS

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,18 @@
+import { dividerClasses } from "@mui/material";
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img
+        src="https://i.imgur.com/PuFDnlw.jpg"
+        alt="Imagem tela inicial"
+        className="img"
+      />
+    </>
+  );
+}
+
+export default Home;
